7defbf880e78935c08ab0d2568c0f912da63d073
Justyna Pogorzelska-Hinc <147341351+JustynaPog@users.noreply.github.com>
2024-12-25 17:12:29 +0100

    Create security_groups.tf

---
+++ security_groups.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow web traffic"
+  vpc_id      = "vpc-xxxxx"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
